c0735d963fecd7311b909be79675c3d53d4eda30
Jonghoon Jin <jhjin0@gmail.com>
2016-01-03 08:37:54 +0800

    automated re-indentations for changes by Jonghoon Jin

--- HardTanh.lua
+++ HardTanh.lua
@@ -1,15 +1,15 @@
 local HardTanh, parent = torch.class('nn.HardTanh', 'nn.Module')
 
 function HardTanh:__init(min_value, max_value)
-   parent.__init(self)
-   self.min_val = min_value or -1
-   self.max_val = max_value or 1
-   assert(self.max_val>self.min_val, 'max_value must be larger than min_value')
+  parent.__init(self)
+  self.min_val = min_value or -1
+  self.max_val = max_value or 1
+  assert(self.max_val>self.min_val, 'max_value must be larger than min_value')
 end
 
 function HardTanh:updateOutput(input)
-   self.min_val = self.min_val or -1
-   self.max_val = self.max_val or 1
+  self.min_val = self.min_val or -1
+  self.max_val = self.max_val or 1
    return input.nn.HardTanh_updateOutput(self, input)
 end
 
