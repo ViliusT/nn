6d49887e8249c043fcf7bb830482e017e30ab777
Michael Rotman <rotmanmi@pc-wolf102>
2016-01-03 08:37:53 +0800

    automated re-indentations for changes by Michael Rotman

--- LeakyReLU.lua
+++ LeakyReLU.lua
@@ -1,18 +1,18 @@
 local LeakyReLU, parent = torch.class('nn.LeakyReLU','nn.Module')
 
 function LeakyReLU:__init(negval,ip)
-   parent.__init(self)
-   if type(negval) == 'boolean' then
-	 local ip = negval
-	 self.negval = 1/100
-   else
-	 self.negval = negval or (1/100)
-   end
-   -- default for inplace is false
-   self.inplace = ip or false
-   if self.negval < 0 then
-      self.inplace = false
-   end
+  parent.__init(self)
+  if type(negval) == 'boolean' then
+    local ip = negval
+    self.negval = 1/100
+  else
+    self.negval = negval or (1/100)
+  end
+  -- default for inplace is false
+  self.inplace = ip or false
+  if self.negval < 0 then
+    self.inplace = false
+  end
 end
 
 function LeakyReLU:__tostring__()
@@ -21,13 +21,13 @@ end
 
 
 function LeakyReLU:updateOutput(input)
-   input.nn.LeakyReLU_updateOutput(self, input)
-   return self.output
+  input.nn.LeakyReLU_updateOutput(self, input)
+  return self.output
 end
 
 function LeakyReLU:updateGradInput(input, gradOutput)
-   input.nn.LeakyReLU_updateGradInput(self, input, gradOutput)
-   return self.gradInput
+  input.nn.LeakyReLU_updateGradInput(self, input, gradOutput)
+  return self.gradInput
 end
 
 
